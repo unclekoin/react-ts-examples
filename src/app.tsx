d81429909bed3a2ee76da6da233ce9a6661c7189
Pavel Koryakin <unclekoin@gmail.com>
2022-03-13 18:00:02 +0300

    feat: add axios & server request

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 import Card, {CardVariant} from "./components/card";
 import UserList from "./components/user-list";
 import {IUser} from "./types/types";
 
 const App = () => {
-    const users: IUser[] = [
-        {
-            "id": 1,
-            "name": "Leanne Graham",
-            "email": "Sincere@april.biz",
-            "address": {
-                "street": "Kulas Light",
-                "city": "Gwenborough",
-                "zipcode": "92998-3874",
-            },
-        },
-        {
-            "id": 2,
-            "name": "Ervin Howell",
-            "email": "Shanna@melissa.tv",
-            "address": {
-                "street": "Victor Plains",
-                "city": "Wisokyburgh",
-                "zipcode": "90566-7771",
-            },
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+            setUsers(response.data);
+        } catch (e) {
+            alert(e);
         }
-    ]
+    }
+
     return (
         <div>
             <Card onClick={(num) => console.log("Click!", num)} variant={CardVariant.outlined} width="200px"
